08c7b502e3301590b86be12361ce1802c37d8cf4
dukekim <46912845+dukekim@users.noreply.github.com>
2020-09-05 22:17:27 +0900

    Add files via upload

--- Src/Hackton_Tizen_01/Hackton_Tizen_01.Tizen/Hackton_Tizen_01.Tizen.cs
+++ Src/Hackton_Tizen_01/Hackton_Tizen_01.Tizen/Hackton_Tizen_01.Tizen.cs
@@ -1,1 +1,25 @@
+using ElmSharp;
+using System;
+using Xamarin.Forms;
 
+namespace Hackton_Tizen_01
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            MainWindow.AvailableRotations = DisplayRotation.Degree_0;
+
+            LoadApplication(new App());
+            Tizen.Log.Debug("BT_TAG", "OnCreate");
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
